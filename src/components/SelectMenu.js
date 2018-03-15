0045a863cee0c34a279fabad90d2a438732918cc
ASA <karoteglathe@gmail.com>
2018-03-15 21:59:21 +0800

    add OptGroups content

--- src/components/SelectMenu.js
+++ src/components/SelectMenu.js
@@ -3,11 +3,26 @@ import styled from 'styled-components';
 
 import SelectMenuItem from './SelectMenuItem';
 
+const OptGroup = styled.div`
+  border-top: 1px solid darkgray;
+
+  &:first-child {
+    border-top: none;
+  }
+`;
+
 class SelectMenu extends Component {
   render() {
+    const { menuDatas } = this.props;
+    console.warn(menuDatas)
+
+    const OptGroupContents = menuDatas.map((group, i) => (
+      <OptGroup key={i}>Group</OptGroup>
+    ));
+
     return (
       <div>
-        Select Menu
+        {OptGroupContents}
       </div>
     );
   }
