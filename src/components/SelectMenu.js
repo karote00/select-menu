316df07b37207a80e7498b64621b0f86e7db8064
ASA <karoteglathe@gmail.com>
2018-03-15 22:16:44 +0800

    add item contents

--- src/components/SelectMenu.js
+++ src/components/SelectMenu.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 import SelectMenuItem from './SelectMenuItem';
 
+const SelectMenuContainer = styled.div`
+  background: rgba(0, 0, 0, .1);
+  border: 1px solid rgba(0, 0, 0, .3);
+`;
+
 const OptGroup = styled.div`
   border-top: 1px solid darkgray;
 
@@ -14,16 +19,28 @@ const OptGroup = styled.div`
 class SelectMenu extends Component {
   render() {
     const { menuDatas } = this.props;
-    console.warn(menuDatas)
 
-    const OptGroupContents = menuDatas.map((group, i) => (
-      <OptGroup key={i}>Group</OptGroup>
-    ));
+    const hasIcon = menuDatas.filter(group => {
+      return group.items.filter(item => item.icon && item.icon.indexOf('fa-') > -1).length > 0;
+    });
+
+    const OptGroupContents = menuDatas.map((group, i) => {
+      const { meta, items } = group;
+      const itemContents = items.map((item, j) => (<SelectMenuItem hasIcon={hasIcon} key={j} {...item} />));
+      const addItem = meta.addable ? <SelectMenuItem icon="fa-plus" /> : null;
+
+      return (
+        <OptGroup key={i}>
+          {itemContents}
+          {addItem}
+        </OptGroup>
+      );
+    });
 
     return (
-      <div>
+      <SelectMenuContainer>
         {OptGroupContents}
-      </div>
+      </SelectMenuContainer>
     );
   }
 }
