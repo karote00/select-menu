803d2e17ed5fd59cfb73a32a8eded47a29093f86
ASA <karoteglathe@gmail.com>
2018-03-15 22:16:54 +0800

    check if must have icon

--- src/components/SelectMenuItem.js
+++ src/components/SelectMenuItem.js
@@ -7,18 +7,22 @@ const propTypes = {
 	icon: PropTypes.string,
 	label: PropTypes.string,
 	controlIcon: PropTypes.string,
+	hasIcon: PropTypes.bool,
 };
 
 const defaultProps = {
 	icon: '',
 	label: '',
 	controlIcon: '',
+	hasIcon: false,
 };
 
 const SelectMenuItemWrapper = styled.div`
 	cursor: pointer;
 	overflow: hidden;
+`;
 
+const SelectMenuItemContentWrapper = styled.div`
 	div {
 		float: left;
 		padding: 4px 8px;
@@ -26,23 +30,31 @@ const SelectMenuItemWrapper = styled.div`
 		&.fl-r {
 			float: right;
 		}
+
+		&.icon {
+			width: 1.6em;
+			padding: 4px 0;
+			text-align: center;
+		}
 	}
 `;
 
 class SelectMenuItem extends Component {
   render() {
-  	const { icon, label, controlIcon } = this.props;
+  	const { icon, label, controlIcon, hasIcon } = this.props;
 
-  	const iconContent = icon.indexOf('fa-') > -1 ? <div><FA name={icon.substr(3)} /></div> : null;
+  	const iconContent = (hasIcon || icon.indexOf('fa-') > -1) ? <div className="icon"><FA name={icon.substr(3)} /></div> : null;
   	const labelContent = <div>{label}</div>;
   	const controlIconContent = controlIcon.indexOf('fa-') > -1 ?
   		<div className="fl-r"><FA name={controlIcon.substr(3)} /></div> : null;
 
     return (
       <SelectMenuItemWrapper>
-      	{iconContent}
-      	{labelContent}
-      	{controlIconContent}
+      	<SelectMenuItemContentWrapper>
+	      	{iconContent}
+	      	{labelContent}
+	      	{controlIconContent}
+	      </SelectMenuItemContentWrapper>
       </SelectMenuItemWrapper>
     );
   }
