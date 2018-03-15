f683a1579b4b443a22ddf32bb0b5986c00414f21
ASA <karoteglathe@gmail.com>
2018-03-15 22:46:20 +0800

    add tips content

--- src/components/SelectMenuItem.js
+++ src/components/SelectMenuItem.js
@@ -20,33 +20,72 @@ const defaultProps = {
 const SelectMenuItemWrapper = styled.div`
 	cursor: pointer;
 	overflow: hidden;
-`;
 
-const SelectMenuItemContentWrapper = styled.div`
-	div {
-		float: left;
-		padding: 4px 8px;
+	.fl-r {
+		float: right;
+	}
 
-		&.fl-r {
-			float: right;
+	.label {
+		&.hasTips {
+			position: relative;
+
+			div {
+				white-space: nowrap;
+		    overflow: hidden;
+		    text-overflow: ellipsis;
+		  }
 		}
+	}
 
-		&.icon {
-			width: 1.6em;
-			padding: 4px 0;
-			text-align: center;
-		}
+	.icon {
+		width: 1.6em;
+		padding: 4px 0;
+		text-align: center;
+	}
+
+	.tips > span {
+		padding: 0 4px;
+		color: rgba(0, 0, 0, .3);
 	}
 `;
 
+const SelectMenuItemContentWrapper = styled.div`
+	> div {
+		float: left;
+		padding: 4px 8px;
+	}
+`;
+
+const tipsItem = (tips) => {
+	let tipsContent = null;
+	switch (typeof tips) {
+		case 'object':
+			if (tips.length) {
+				tipsContent = tips.map((tip, i) => {
+					if (typeof tip === 'string' && tip.indexOf('fa-') > -1) return <FA key={i} name={tip.substr(3)} />
+					return (<span key={i}>{tip}</span>);
+				});
+			}
+			break;
+		default:
+			tipsContent = (<span>{tips}</span>);
+			break;
+	}
+
+	return tipsContent;
+};
+
 class SelectMenuItem extends Component {
   render() {
-  	const { icon, label, controlIcon, hasIcon } = this.props;
+  	const { icon, label, controlIcon, hasIcon, tips } = this.props;
 
-  	const iconContent = (hasIcon || icon.indexOf('fa-') > -1) ? <div className="icon"><FA name={icon.substr(3)} /></div> : null;
-  	const labelContent = <div>{label}</div>;
+  	const iconContent = (hasIcon || icon.indexOf('fa-') > -1) ?
+  		<div className="icon"><FA name={icon.substr(3)} /></div> : null;
+  	const labelContent = <div className={`label ${tips ? 'hasTips' : ''}`}><div>{label}</div></div>;
   	const controlIconContent = controlIcon.indexOf('fa-') > -1 ?
   		<div className="fl-r"><FA name={controlIcon.substr(3)} /></div> : null;
+  	const tipsContent = tipsItem(tips);
+
 
     return (
       <SelectMenuItemWrapper>
@@ -54,6 +93,7 @@ class SelectMenuItem extends Component {
 	      	{iconContent}
 	      	{labelContent}
 	      	{controlIconContent}
+	      	<div className="fl-r tips">{tipsContent}</div>
 	      </SelectMenuItemContentWrapper>
       </SelectMenuItemWrapper>
     );
