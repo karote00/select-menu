277fc06b15044f81597a36bc07a205701b23f572
ASA <karoteglathe@gmail.com>
2018-03-16 22:58:33 +0800

    remove SelectMenu in App testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SelectMenuItem from './components/SelectMenuItem';
 
 class App extends Component {
   render() {
@@ -14,8 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <SelectMenuItem />
-        <SelectMenuItem primary />
       </div>
     );
   }
