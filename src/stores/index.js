aef0fff9962ea8b437d757e8dbeefa55fe4587f8
ASA <karoteglathe@gmail.com>
2018-03-17 22:22:59 +0800

    add editable parameter

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,11 +3,41 @@ import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
 const menuItems = {
-	1: { itemKey: 1, label: 'rocket', tips: [100, 200, 'fa-mobile'], selectable: true, selected: false },
-  2: { itemKey: 2, label: 'smiley', disabled: true, selectable: true, selected: false },
-  3: { itemKey: 3, icon: 'fa-stop', label: 'park', tips: [10, 20, 'fa-car'] },
-  4: { itemKey: 4, icon: 'fa-play', label: 'play', tips: ['fa-mobile'] },
-  5: { itemKey: 5, label: 'America', editabled: true, selectable: true, selected: false, subMenu: 1},
+	1: {
+		itemKey: 1,
+		label: 'rocket',
+		tips: [100, 200, 'fa-mobile'],
+		selectable: true,
+		selected: false,
+	},
+  2: {
+  	itemKey: 2,
+  	label: 'smiley',
+  	disabled: true,
+  	selectable: true,
+  	selected: false,
+  	editable: true,
+  },
+  3: {
+  	itemKey: 3,
+  	icon: 'fa-stop',
+  	label: 'park',
+  	tips: [10, 20, 'fa-car'],
+  },
+  4: {
+  	itemKey: 4,
+  	icon: 'fa-play',
+  	label: 'play',
+  	tips: ['fa-mobile'],
+  },
+  5: {
+  	itemKey: 5,
+  	label: 'America',
+  	editabled: true,
+  	selectable: true,
+  	selected: false,
+  	subMenu: 1,
+  },
 };
 
 const initialState = {
