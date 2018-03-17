cb6a0a476552036d571623eb4c52ff7d6844b6cb
ASA <karoteglathe@gmail.com>
2018-03-17 21:27:09 +0800

    refactor data structor

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,27 +2,36 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
-const initialState = [
-  {
-    meta: {
-      addable: false,
-    },
-    items: [
-      { icon: 'fa-rocket', label: 'rocket', tips: [100, 200, 'fa-mobile'] },
-      { label: 'smiley', disabled: true },
-    ]
+const menuItems = {
+	1: { itemKey: 1, label: 'rocket', tips: [100, 200, 'fa-mobile'], selectable: true, selected: false },
+  2: { itemKey: 2, label: 'smiley', disabled: true, selectable: true, selected: false },
+  3: { itemKey: 3, icon: 'fa-stop', label: 'park', tips: [10, 20, 'fa-car'] },
+  4: { itemKey: 4, icon: 'fa-play', label: 'play', tips: ['fa-mobile'] },
+  5: { itemKey: 5, label: 'America', editabled: true, selectable: true, selected: false, subMenu: 1},
+};
+
+const initialState = {
+	main: [{
+		meta: {
+			addable: false,
+		},
+		items: [1, 2],
+	}, {
+		meta: {
+			addable: false,
+		},
+		items: [3, 4, 5],
+	}],
+  sub: {
+  	1: [{
+  		meta: {
+  			addable: false,
+  		},
+  		items: [2, 5],
+  	}],
   },
-  {
-    meta: {
-      addable: false,
-    },
-    items: [
-      { icon: 'fa-stop', label: 'park', tips: [10, 20, 'fa-car'] },
-      { icon: 'fa-play', label: 'play', tips: ['fa-mobile'] },
-      { label: 'America', editabled: true },
-    ]
-  }
-];
+  menuItems,
+};
 // const initialState = {};
 
 const store = createStore(
