c5bed11e4c9ede9b5b7c4b6ef13ab9a5f2fc26d6
ASA <karoteglathe@gmail.com>
2018-03-19 00:10:04 +0800

    add keyCode constant

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const NUMBER_KEYCODE =
+  [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105];
+
+export const CHAR_KEYCODE =
+  [
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+    78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+  ];
+
+const constants = {
+  NUMBER_KEYCODE,
+  CHAR_KEYCODE,
+};
+
+export default constants;
