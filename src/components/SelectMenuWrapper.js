4bcc4c2974ea1c316023452ac64562de0787e3b9
ASA <karoteglathe@gmail.com>
2018-03-21 21:43:42 +0800

    remove state control

--- src/components/SelectMenuWrapper.js
+++ src/components/SelectMenuWrapper.js
@@ -9,7 +9,6 @@ import SelectMenu from './SelectMenu';
 import '../css/SelectMenu.css';
 
 const propTypes = {
-  focusedItem: PropTypes.string,
 };
 
 const defaultProps = {
@@ -32,34 +31,8 @@ const Wrapper = styled.div`
 `;
 
 class SelectMenuWrapper extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectMenuIsOpen: false,
-      selectedItems: [],
-    };
-
-    this.onFocusChange = this.onFocusChange.bind(this);
-    this.onOpen = this.onOpen.bind(this);
-    this.onClose = this.onClose.bind(this);
-  }
-
-  onFocusChange(focusedItem) {
-    this.setState({ focusedItem });
-  }
-
-  onOpen() {
-    this.setState({ selectMenuIsOpen: true });
-  }
-
-  onClose() {
-    this.setState({ selectMenuIsOpen: false });
-  }
-
   render() {
     const { menuData } = this.props;
-    const { selectMenuIsOpen } = this.state;
 
     const menu = menuData.main.map(m => ({
       ...m,
@@ -73,7 +46,6 @@ class SelectMenuWrapper extends Component {
         />
         <SelectMenu
           layer={0}
-          isOpen={selectMenuIsOpen}
           menu={menu}
         />
       </Wrapper>
